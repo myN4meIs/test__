1696eabc794cf4fc72ae355c1dc8f3eb7884fdc8
d06i <llll@DESKTOP-C8VGJLV>
2025-02-16 13:45:46 +0300

    CMakeList updated with optimization flag and test code for C++20 support.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 
+#include <format>
+
+auto cpp20_support_test(const std::string& str) {
+
+	std::string f = std::format("****** {} *********", str);
+	return f;
+}
+
 int main(){
   
   int a = 123;
   float b = 12.4f;
   double c = a * b;
 
-  std::cout << c ;
+  std::cout << static_cast<int>(c) << "\n" 
+			<< cpp20_support_test("IF HAD TO") ;
 
   return 0;
 }
